90f7ae1cf1844207cece82c2947eb4bf910ff0ce
pabloescobar-reborn <yogivangala07@gmail.com>
2025-11-03 18:58:58 +0000

    sync with device tree

--- sm8450-common-vendor.mk
+++ sm8450-common-vendor.mk
@@ -280,13 +280,10 @@ PRODUCT_PACKAGES += \
     lib-rtpcommon \
     lib-rtpcore \
     lib-rtpsl \
-    libAlacSwDec \
-    libApeSwDec \
     libEvrcSwCodec \
     libFace3DTA \
     libFace3D_hlos \
     libFileMux_proprietary \
-    libFlacSwDec \
     libGPQTEEC_vendor \
     libGPTEE_vendor \
     libGPreqcancel \
